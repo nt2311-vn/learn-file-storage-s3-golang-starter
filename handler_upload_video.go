c6f7ad81e19f5f860df81dba396939cbefaf7b52
nt2311 <nguyentoan231196@gmail.com>
2025-01-04 16:36:54 +0700

    make the handler upload video

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -2,6 +2,43 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/google/uuid"
+
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	const uploadLimit = 1 << 30
+	r.Body = http.MaxBytesReader(w, r.Body, uploadLimit)
+
+	vidIDString := r.PathValue("videoID")
+	vidID, err := uuid.Parse(vidIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Coundn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Could not validate JWT", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(vidID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not find video", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not authorized this video", err)
+		return
+	}
+}
