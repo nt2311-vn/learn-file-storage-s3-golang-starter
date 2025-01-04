7d22cb47dd386048996216a34a3ec3ab78632640
nt2311 <nguyentoan231196@gmail.com>
2025-01-04 22:04:19 +0700

    implement handler_upload_video

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"io"
+	"mime"
 	"net/http"
+	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/google/uuid"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -41,4 +46,68 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusUnauthorized, "Not authorized this video", err)
 		return
 	}
+
+	file, handler, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	defer file.Close()
+
+	mediaType, _, err := mime.ParseMediaType(handler.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid content-type", err)
+		return
+	}
+
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "Invalid file typpe only mp4 is allowed", err)
+		return
+	}
+
+	tempFile, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create temp file", err)
+		return
+	}
+
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	if _, err := io.Copy(tempFile, file); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not write file to disk", err)
+		return
+	}
+
+	_, err = tempFile.Seek(0, io.SeekStart)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not reset file pointer", err)
+		return
+	}
+
+	key := getAssetPath(mediaType)
+	_, err = cfg.s3Client.PutObject(
+		r.Context(),
+		&s3.PutObjectInput{
+			Bucket:      aws.String(cfg.s3Bucket),
+			Key:         aws.String(key),
+			Body:        tempFile,
+			ContentType: aws.String(mediaType),
+		},
+	)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error uploading file in S3", err)
+		return
+	}
+
+	url := cfg.getObjectURL(key)
+	video.VideoURL = &url
+
+	if err = cfg.db.UpdateVideo(video); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
